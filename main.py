e2e45ae71cd3e17af40a5cccbb9c31898c6657d9
Colm Murphy <116750963+cmurphy012@users.noreply.github.com>
2023-02-28 16:09:24 +0000

    Update main.py
    Added f-strings to print user and computers' choices

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import random
 
-
 def play():
     user = [input("'r' for Rock, 'p' for Paper, or 's' for scissors: \n")]
-    
-    if user != 'r' or 'p' or 's':
-        print("Invalid Input!")
+
+    if user == ['r'] or user == ['p'] or user == ['s']:
+        computer = random.choices(['r', 'p', 's'])
+        
+        print(f"You chose {user}")
+        print(f"The computer chose {computer}")
+
+        if user == computer:
+            print("It's a tie!")
+            return 0
+
+        elif is_win(user, computer):
+            print("You win!")
+            return 0
+
+        print("You lose!")
+        return 0
+    else:
+        print("Invalid input!")
         exit()
-
-    computer = random.choices(['r', 'p', 's'])
-    #print(user)
-    #print(computer)
-
-    if user == computer:
-        print("It's a tie!")
-        return 0
-
-    elif is_win(user, computer):
-        print("You win!")
-        return 0
-
-    print("You lose!")
-    return 0
 
 
 def is_win(player, opponent):
